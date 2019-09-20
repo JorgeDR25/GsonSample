1955c31ff3d6f8da02e42f597e4becf8392a42eb
unknown <jorgesur1999@gmail.com>
2019-09-20 11:54:02 +0100

    aiuda

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner entrada=new Scanner(System.in);
+		boolean comprueba=true;
+		
+		
+		
+		Persona p=new Persona();
+		
+		while(comprueba) {
+		try{
+			System.out.println("Introduce nombre");
+			p.setNombre(entrada.nextLine());
+			System.out.println("Introduce apellidos");
+			p.setApellidos(entrada.nextLine());
+			System.out.println("Introduce edad");
+			p.setEdad(entrada.nextInt());
+			comprueba=false;
+		}catch(Exception Exception) {
+			System.out.println("Error, vuelva a introducir los datos");
+		}
+		}
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(p);
+		System.out.println(json);
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/Persona.java
@@ -0,0 +1,43 @@
+public class Persona {
+
+	public String nombre,apellidos;
+	public int edad=0;
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Persona() {
+		
+		
+		
+		
+	}
+	
+	
+	
+
+
+
+
+}
